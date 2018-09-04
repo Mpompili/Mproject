9983a88a16813b555d664e10afcf60aa7a77b5c8
Michael Pompili <michael@churroboros.com>
2018-09-04 16:17:34 -0700

    part1 of validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,6 +5,10 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
 const passport = require('passport');
+
+// Load Input Validation
+const validateRegisterInput = require('../../validation/register');
+
 // Get User model.
 const User = require('../../models/User');
 
@@ -21,13 +25,19 @@ router.get('/test', (req, res) =>
 // @desc    Register user
 // @access  Public
 router.post('/register', (req, res) => {
+    const { errors, isValid } = validateRegisterInput(req.body);
+
+    // Check Validation
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+
     User.findOne({
         email: req.body.email
     }).then(user => {
         if (user) {
-            return res.status(400).json({
-                email: 'Email already exists'
-            });
+            errors.email = 'Email already exists';
+            return res.status(400).json(errors);
         } else {
             const avatar = gravatar.url(req.body.email, {
                 s: '200', // size
