c9be594640fcfdaec43a8abc55205fd4c62b9844
Michael Pompili <michael@churroboros.com>
2018-09-13 15:00:39 -0700

    continue post api

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,12 +1,43 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+const Post = require('../../models/Post');
+
+// Validation
+const validatePostInput = require('../../validation/post');
 
 // @route   GET api/posts/test
-// @desc    Tests post route 
-// @access  Public 
-router.get('/test', (req, res) => res.json({
-    msg: 'Posts Works'
-}));
+// @desc    Tests post route
+// @access  Public
+router.get('/test', (req, res) =>
+    res.json({
+        msg: 'Posts Works'
+    })
+);
 
+// @route   POST api/posts/
+// @desc    Create a new post
+// @access  Private
+router.post(
+    '/',
+    passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+        const { errors, isValid } = validatePostInput(req.body);
+
+        if (!isValid) {
+            return res.status(400).json(errors);
+        }
+        const newPost = new Post({
+            text: req.body.text,
+            name: req.body.name,
+            avatar: req.body.name,
+            user: req.user.id
+        });
+
+        newPost.save().then(post => res.json(post));
+    }
+);
 
 module.exports = router;
