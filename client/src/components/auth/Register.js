424ae65aa7b622f1e565a85c5585c0b454a3beb5
Michael Pompili <michael@churroboros.com>
2018-09-19 16:21:02 -0700

    hydrate register

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,10 +1,96 @@
 import React, { Component } from 'react';
 
 export default class Register extends Component {
+    constructor() {
+        super();
+        this.state = {
+            name: '',
+            email: '',
+            password: '',
+            password2: '',
+            errors: {}
+        };
+        this.updateField = this.updateField.bind(this);
+        this.submit = this.submit.bind(this);
+    }
+    updateField(e) {
+        this.setState({ [e.target.name]: e.target.value });
+    }
+
+    submit(e) {
+        e.preventDefault();
+        const newUser = {
+            name: this.state.name,
+            email: this.state.email,
+            password: this.state.password,
+            password2: this.state.password2
+        };
+        console.log(newUser);
+    }
     render() {
         return (
-            <div>
-                <h1>Register</h1>
+            <div className="register">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-8 m-auto">
+                            <h1 className="display-4 text-center">Sign Up</h1>
+                            <p className="lead text-center">
+                                Create your DevConnector account
+                            </p>
+                            <form action="create-profile.html">
+                                <div className="form-group">
+                                    <input
+                                        onChange={this.updateField}
+                                        type="text"
+                                        className="form-control form-control-lg"
+                                        placeholder="Name"
+                                        name="name"
+                                        value={this.state.name}
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <input
+                                        onChange={this.updateField}
+                                        type="email"
+                                        className="form-control form-control-lg"
+                                        placeholder="Email Address"
+                                        name="email"
+                                        value={this.state.email}
+                                    />
+                                    <small className="form-text text-muted">
+                                        This site uses Gravatar so if you want a
+                                        profile image, use a Gravatar email
+                                    </small>
+                                </div>
+                                <div className="form-group">
+                                    <input
+                                        onChange={this.updateField}
+                                        type="password"
+                                        className="form-control form-control-lg"
+                                        placeholder="Password"
+                                        name="password"
+                                        value={this.state.password}
+                                    />
+                                </div>
+                                <div className="form-group">
+                                    <input
+                                        onChange={this.updateField}
+                                        type="password"
+                                        className="form-control form-control-lg"
+                                        placeholder="Confirm Password"
+                                        name="password2"
+                                        value={this.state.password2}
+                                    />
+                                </div>
+                                <input
+                                    onClick={this.submit}
+                                    type="submit"
+                                    className="btn btn-info btn-block mt-4"
+                                />
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
